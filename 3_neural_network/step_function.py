5f67fd50877b5f39eb242e0de198694693a79926
kazuma <kazuma0204kk@gmail.com>
2025-03-17 11:53:04 +0900

    3 neural network

---
+++ 3_neural_network/step_function.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def step_function( x ):
+      
+    return np.array( x > 0, dtype=np.int64 )
+
+
+x = np.arange(-5.0, 5.0, 0.1)
+print(type(x))
+y= step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1) # Y軸の範囲
+plt.show()
